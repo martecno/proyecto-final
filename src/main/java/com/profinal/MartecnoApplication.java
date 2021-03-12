6a88fff6fe149c47300f2067e486c487b6675b18
Bruno Marandola <43514890+martecno@users.noreply.github.com>
2021-03-12 20:41:01 -0300

    Change name of proyect

---
+++ src/main/java/com/profinal/MartecnoApplication.java
@@ -0,0 +1,13 @@
+package com.profinal.martecno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartecnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartecnoApplication.class, args);
+	}
+
+}
