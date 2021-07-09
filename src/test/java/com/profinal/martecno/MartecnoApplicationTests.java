b7ed584b64316e285c766c31806b83a9fbd2851d
martecno <bruno@martecno.com.ar>
2021-07-09 16:37:50 -0300

    Controller point

---
+++ src/test/java/com/profinal/martecno/MartecnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profinal.martecno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartecnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
