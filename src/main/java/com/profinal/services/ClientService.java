18576e210590a9898da09cabd4acc9e6a1754323
martecno <bruno@martecno.com.ar>
2021-07-17 11:31:24 -0300

    Adding FindAll

--- src/main/java/com/profinal/services/ClientService.java
+++ src/main/java/com/profinal/services/ClientService.java
@@ -1,5 +1,7 @@
 package com.profinal.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,4 +20,9 @@ public class ClientService {
 	public Client save(Client client) {
 		return clientRepository.saveAndFlush(client);
 	}
+
+	public List<Client> getClients() {
+		return clientRepository.findAll();
+	}
+
 }
